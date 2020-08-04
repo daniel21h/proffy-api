ba0847640e21f5f6ebba9e059c4e637ef381d7aa
daniel21h <hesseldaniel21@gmail.com>
2020-08-04 20:55:28 -0300

    feat: create index filters

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -22,7 +22,20 @@ export default class ClassesController {
     const timeInMinutes = convertHourToMinutes(filters.time as string)
 
     const classes = await db('classes')
+      .whereExists(function() {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw(
+            '`class_schedule`.`week_day` = ??', 
+            [Number(filters.week_day as string)]
+          )
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
       .where('classes.subject', '=', filters.subject as string)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*'])
 
     return response.json(classes)
   }
