aa69de4b4b83d61a6c13dce1f4e7714a65b217a6
daniel21h <hesseldaniel21@gmail.com>
2021-01-22 14:32:19 -0300

    feat: prepared to deploy

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ server.use(cors())
 server.use(express.json())
 server.use(routes)
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
   console.log('>> Server running on port 3333. <<')
 })
