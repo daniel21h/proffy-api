9c77adf4eb5f81830d397919304de25f0e530023
daniel21h <hesseldaniel21@gmail.com>
2021-01-23 09:28:11 -0300

    fix: sending o deploy

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        filename: path.resolve(__dirname, 'dist', 'database', 'database.sqlite')
     },
     migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
     },
     useNullAsDefault: true,
 };
